7027ea3e50c4f7753d2b1a08d289bbec68f8abdc
Vaclav Pelisek <peldax@gmail.com>
2016-12-06 01:22:22 +0100

    added actor list

---
+++ app/component/Entity/AuthorList/ActorList.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Component;
+
+class ActorList extends BaseDatagridComponent
+{
+    protected $movieModel;
+
+    public function __construct(
+        \App\Model\PersonModel $personModel,
+        \App\Model\MovieModel $movieModel)
+    {
+        parent::__construct();
+
+        $this->model = $personModel;
+        $this->movieModel = $movieModel;
+    }
+
+    public function createComponentDataGrid()
+    {
+        parent::createComponentDataGrid();
+
+        $this->grid->addColumn('name', 'Name')->enableSort();
+        $this->grid->addColumn('country_id', 'Nationality')->enableSort();
+        $this->grid->addColumn('movie_count', '# of movies');
+        $this->grid->addColumn('average_rating', 'Average rating');
+        $this->grid->addColumn('top_movie', 'Top movie');
+
+        $this->grid->addCellsTemplate(__DIR__ . '/ActorListCellsTemplate.latte');
+        $this->grid->setTemplateParams(array('movieModel' => $this->movieModel));
+
+        return $this->grid;
+    }
+
+
+    public function getDataSource($filter, $order)
+    {
+        $filters = array();
+        $filters[':movie2actor.id NOT'] = null;
+
+        foreach ($filter as $k => $v)
+        {
+            if ($k == 'id' || is_array($v))
+            {
+                $filters[$k] = $v;
+            }
+            else if ($k == 'name')
+            {
+                $filters['name LIKE ?'] = "%$v%";
+                $filters['surname LIKE ?'] = "%$v%";
+            }
+            else
+            {
+                $filters[$k.' LIKE ?'] = "%$v%";
+            }
+        }
+
+        $set = $this->model->findByArray($filters);
+
+        if ($order[0])
+        {
+            if ($order[0] == 'name')
+            {
+                $orders = "surname $order[1], name $order[1]";
+            }
+            else if ($order[0] == 'country_id')
+            {
+                $orders = "country.name $order[1]";
+            }
+            else if ($order[0] == 'average_rating')
+            {
+                $orders = "TODO $order[1]";
+            }
+            else if ($order[0] == 'movie_count')
+            {
+                $orders = "TODO $order[1]";
+            }
+            else if ($order[0] == 'top_movie')
+            {
+                $orders = "TODO $order[1]";
+            }
+            else
+            {
+                $orders = implode(' ', $order);
+            }
+            $set->order($orders);
+        }
+
+        return $set;
+    }
+}
--- app/model/MovieModel.php
+++ app/model/MovieModel.php
@@ -25,7 +25,7 @@ final class MovieModel extends BaseModel
         $this->movieActorModel = $movieActorModel;
     }
 
-    public function getTopMovie($directorId)
+    public function getTopDirectorMovie($directorId)
     {
         return $this->query(
         "SELECT
@@ -38,7 +38,20 @@ final class MovieModel extends BaseModel
         ORDER BY sum(rating_movie.rating) DESC")->fetch();
     }
 
-    public function getAverageRating($directorId)
+    public function getTopActorMovie($actorId)
+    {
+        return $this->query(
+            "SELECT
+        movie.*
+        FROM movie
+        LEFT JOIN rating_movie ON movie.id = rating_movie.movie_id
+        LEFT JOIN movie2actor ON movie.id = movie2actor.movie_id
+        WHERE movie2actor.person_id = {$actorId}
+        GROUP BY movie.id
+        ORDER BY sum(rating_movie.rating) DESC")->fetch();
+    }
+
+    public function getAverageDirectorRating($directorId)
     {
         $result = $this->ratingMovieModel->query(
         "SELECT
@@ -51,6 +64,25 @@ final class MovieModel extends BaseModel
         FROM movie2director
         LEFT JOIN rating_movie ON rating_movie.movie_id = movie2director.movie_id
         WHERE movie2director.person_id = {$directorId}
+        GROUP BY rating_movie.movie_id
+        ) AS `subquery`")->fetch();
+
+        return $result->size ? $result->sum / $result->size : null;
+    }
+
+    public function getAverageActorRating($actorId)
+    {
+        $result = $this->ratingMovieModel->query(
+            "SELECT
+        sum(`subsum`) AS `sum`,
+        count(*) AS `size`
+        FROM
+        (
+        SELECT 
+        (sum(rating_movie.rating) / count(*)) as `subsum`
+        FROM movie2actor
+        LEFT JOIN rating_movie ON rating_movie.movie_id = movie2actor.movie_id
+        WHERE movie2actor.person_id = {$actorId}
         GROUP BY rating_movie.movie_id
         ) AS `subquery`")->fetch();
 
