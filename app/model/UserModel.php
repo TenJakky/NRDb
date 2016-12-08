23f8782fba13d69fe47e1c2a46263af0e023034c
Vaclav Pelisek <peldax@gmail.com>
2016-12-09 00:48:36 +0100

    fixed select for statistics

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -10,24 +10,51 @@ class UserModel extends BaseModel
     {
         return $this->query(
         "SELECT
-        *,
-        `movie` + `series` + `book` + `game` as `total`
-        FROM
-        (
-        SELECT
         user.id,
         user.username,
-        count(rating_movie.id) as `movie`,
-        count(rating_series.id) as `series`,
-        count(rating_book.id) as `book`,
-        count(rating_game.id) as `game`
+        `movie`,
+        `series`,
+        `book`,
+        `game`,
+        `movie` + `series` + `book` + `game` as `total`
+        FROM user
+        
+        LEFT JOIN (
+        SELECT
+        user.id AS `id`,
+        count(rating_movie.id) as `movie`
         FROM user
         LEFT JOIN rating_movie on rating_movie.user_id = user.id
+        GROUP BY user.id
+        ) AS `sq_movie` ON user.id = `sq_movie`.`id`
+        
+        LEFT JOIN (
+        SELECT
+        user.id AS `id`,
+        count(rating_series.id) as `series`
+        FROM user
         LEFT JOIN rating_series on rating_series.user_id = user.id
+        GROUP BY user.id
+        ) AS `sq_series` ON user.id = `sq_series`.`id`
+        
+        LEFT JOIN (
+        SELECT
+        user.id AS `id`,
+        count(rating_book.id) as `book`
+        FROM user
         LEFT JOIN rating_book on rating_book.user_id = user.id
+        GROUP BY user.id
+        ) AS `sq_book` ON user.id = `sq_book`.`id`
+        
+        LEFT JOIN (
+        SELECT
+        user.id AS `id`,
+        count(rating_game.id) as `game`
+        FROM user
         LEFT JOIN rating_game on rating_game.user_id = user.id
         GROUP BY user.id
-        ) as `subquery`
+        ) AS `sq_game` ON user.id = `sq_game`.`id`
+        
         ORDER BY `total` DESC
         LIMIT 5")->fetchAll();
     }
