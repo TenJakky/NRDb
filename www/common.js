29bfee7cf85b89fe6f0d6c621e43e73dd11999db
Vaclav Pelisek <peldax@gmail.com>
2016-12-10 04:12:56 +0100

    overriding nette's showFormErrors function

--- www/common.js
+++ www/common.js
@@ -9,3 +9,25 @@ $(document).ready(function ()
 {
     $('select').chosen(chosenOptions);
 });
+
+Nette.showFormErrors = function (form, errors)
+{
+    $(form).find('span.form-error').remove();
+
+    for (var i = 0; i < errors.length; i++)
+    {
+        $(errors[i].element).parent().append('<span class="form-error">' + errors[i].message + '</span>');
+    }
+
+    if (errors.length > 0)
+    {
+        var elem = errors[0].element;
+
+        if (elem.nodeName == 'SELECT')
+        {
+            $(elem).trigger('chosen:activate');
+            return;
+        }
+        $(elem).focus();
+    }
+};
