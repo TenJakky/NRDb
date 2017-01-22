fe65744ef992b959b3daaf5c67fb6a5e640ef791
Vaclav Pelisek <peldax@gmail.com>
2017-01-22 01:37:27 +0100

    fixes with entity view

--- app/model/Entity/SeriesModel.php
+++ app/model/Entity/SeriesModel.php
@@ -22,10 +22,23 @@ final class SeriesModel extends BaseEntityModel
     public function getActors($id)
     {
         return $this->query("
-        SELECT person.id FROM {$this->tableName}
-        LEFT JOIN  ON ent_season.series_id = ent_series.id
+        SELECT person.id AS person_id
+        FROM {$this->tableName}
+        LEFT JOIN ent_season ON ent_season.series_id = ent_series.id
         LEFT JOIN jun_season2actor ON jun_season2actor.season_id = ent_season.id
         RIGHT JOIN person on person.id = jun_season2actor.person_id
+        WHERE ent_series.id = ?
+        GROUP BY person.id", $id);
+    }
+
+    public function getDirectors($id)
+    {
+        return $this->query("
+        SELECT person.id AS person_id
+        FROM {$this->tableName}
+        LEFT JOIN ent_season ON ent_season.series_id = ent_series.id
+        LEFT JOIN jun_season2director ON jun_season2director.season_id = ent_season.id
+        RIGHT JOIN person on person.id = jun_season2director.person_id
         WHERE ent_series.id = ?
         GROUP BY person.id", $id);
     }
