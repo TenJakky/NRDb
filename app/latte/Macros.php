ad7e9760a26e253d8879cf598aeb571c1cfef9d6
Vaclav Pelisek <peldax@gmail.com>
2017-04-04 00:28:25 +0200

    remove unnecessary macros

--- app/latte/Macros.php
+++ app/latte/Macros.php
@@ -13,37 +13,11 @@ class Macros extends \Latte\Macros\MacroSet
             echo "<ul class=\"artist-list\">";
             foreach (%node.word as $temp)
             {
-                if (isset($temp->person_id))
-                {
-                    echo 
-                    \'<li><a href="\'.
-                    LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Person:view", [$temp->person_id])).
-                    \'">\'.
-                    LR\Filters::escapeHtmlText(call_user_func($this->filters->person, $temp->person_id)).
-                    \'</a></li>\';
-                }
-                else
-                    echo 
-                    \'<li><a href="\'.
-                    LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Group:view", [$temp->group_id])).
-                    \'">\'.
-                    $temp->group->name.
-                    \'</a></li>\';
-            }
-            echo "</ul>";
-            ');
-        });
-        $set->addMacro('personList', function($node, $writer)
-        {
-            return $writer->write('
-            echo "<ul class=\"artist-list\">";
-            foreach (%node.word as $temp)
-            {
                 echo 
                 \'<li><a href="\'.
-                LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Person:view", [$temp->id])).
+                LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Artist:view", [$temp->id])).
                 \'">\'.
-                LR\Filters::escapeHtmlText(call_user_func($this->filters->person, $temp->id)).
+                LR\Filters::escapeHtmlText(call_user_func($this->filters->artist, $temp->id)).
                 \'</a></li>\';
             }
             echo "</ul>";
@@ -57,25 +31,9 @@ class Macros extends \Latte\Macros\MacroSet
             {
                 echo 
                 \'<li><a href="\'.
-                LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Person:view", [$temp->person_id])).
+                LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Artist:view", [$temp->member_id])).
                 \'">\'.
-                LR\Filters::escapeHtmlText(call_user_func($this->filters->person, $temp->person->id)).
-                \'</a></li>\';
-            }
-            echo "</ul>";
-            ');
-        });
-        $set->addMacro('bandList', function($node, $writer)
-        {
-            return $writer->write('
-            echo "<ul class=\"artist-list\">";
-            foreach (%node.word as $temp)
-            {
-                echo 
-                \'<li><a href="\'.
-                LR\Filters::escapeHtmlAttr($this->global->uiPresenter->link("Group:view", [$temp->group_id])).
-                \'">\'.
-                $temp->group->name.
+                LR\Filters::escapeHtmlText(call_user_func($this->filters->artist, $temp->member_id)).
                 \'</a></li>\';
             }
             echo "</ul>";
