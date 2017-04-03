34ac1553af404637dd59f309061413c15d47940e
Vaclav Pelisek <peldax@gmail.com>
2017-04-04 00:29:43 +0200

    enahnced statistic components

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -5,4 +5,9 @@ namespace App\Model;
 class UserModel extends BaseModel
 {
     public $tableName = 'user';
+
+    public function getMaxRatings($category)
+    {
+        return $this->getTable()->select("ratings_$category AS count")->order("ratings_$category DESC")->limit(1)->fetch()->count;
+    }
 }
