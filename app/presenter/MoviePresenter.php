5817b41c1b3679a5ea25e84af98508c223144265
Vaclav Pelisek <peldax@gmail.com>
2017-04-08 01:27:34 +0200

    update presenterů

--- app/presenter/MoviePresenter.php
+++ app/presenter/MoviePresenter.php
@@ -4,13 +4,5 @@ namespace App\Presenter;
 
 final class MoviePresenter extends BaseEntityPresenter
 {
-    protected $ratingModel;
 
-    public function __construct(
-        \App\Model\MovieModel $movieModel,
-        \App\Model\RatingMovieModel $ratingMovieModel)
-    {
-        $this->model = $movieModel;
-        $this->ratingModel = $ratingMovieModel;
-    }
 }
--- app/presenter/SeriesPresenter.php
+++ app/presenter/SeriesPresenter.php
@@ -4,13 +4,5 @@ namespace App\Presenter;
 
 final class SeriesPresenter extends BaseEntityPresenter
 {
-    protected $ratingModel;
 
-    public function __construct(
-        \App\Model\SeriesModel $seriesModel,
-        \App\Model\RatingSeriesModel $ratingSeriesModel)
-    {
-        $this->model = $seriesModel;
-        $this->ratingModel = $ratingSeriesModel;
-    }
 }
