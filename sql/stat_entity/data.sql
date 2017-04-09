f17691b5ccfb33a66360f101f9aa90a85604e984
Vaclav Pelisek <peldax@gmail.com>
2017-04-09 23:36:13 +0200

    default data for stat tables

---
+++ sql/stat_entity/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `stat_entity` (`id`, `type`, `count`, `rating_count`) VALUES
+  (1, 'movie', 0, 0),
+  (2, 'series', 0, 0),
+  (3, 'season', 0, 0),
+  (4, 'book', 0, 0),
+  (5, 'music', 0, 0),
+  (6, 'game', 0, 0);
