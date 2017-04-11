2780d22220e33ef5d4a3a279157cd88bed4abf30
Vaclav Pelisek <peldax@gmail.com>
2017-04-11 19:50:14 +0200

    more translations

--- app/locale/en.php
+++ app/locale/en.php
@@ -3,16 +3,31 @@
 return [
     'movie' => 'Movie',
     'movie_pl' => 'Movies',
+    'movie_add' => 'Add movie',
+    'movie_edit' => 'Edit movie',
+    'movie_list' => 'List of movies',
     'series' => 'Series',
     'series_pl' => 'Series',
+    'series_add' => 'Add series',
+    'series_edit' => 'Edit series',
+    'series_list' => 'List of series',
     'season' => 'Season',
     'season_pl' => 'Seasons',
     'book' => 'Book',
     'book_pl' => 'Books',
+    'book_add' => 'Add book',
+    'book_edit' => 'Edit book',
+    'book_list' => 'List of books',
     'music' => 'Music',
     'music_pl' => 'Music',
+    'music_add' => 'Add music',
+    'music_edit' => 'Edit music',
+    'music_list' => 'List of music',
     'game' => 'Game',
     'game_pl' => 'Games',
+    'game_add' => 'Add game',
+    'game_edit' => 'Edit game',
+    'game_list' => 'List of games',
 
     'artist' => 'Artist',
     'artist_pl' => 'Artists',
@@ -24,11 +39,13 @@ return [
     'categories' => 'Categories',
     'people' => 'People',
     'stats' => 'Statistics',
+    'settings' => 'Settings',
     'logout' => 'Logout',
     'login' => 'Login',
     'credits' => 'Credits',
     'lang' => 'Language',
 
+    // locales
     'en' => 'English',
     'cs' => 'Czech',
     'de' => 'German',
@@ -36,22 +53,13 @@ return [
     // default:default
     'news' => 'News',
     'intro' => 'Introduction',
-    
-    // default:settings
 
     // default:credits
-
-    // entity:
-    'movie_add' => 'Add movie',
-    'movie_list' => 'List of movies',
-    'series_add' => 'Add series',
-    'series_list' => 'List of series',
-    'book_add' => 'Add book',
-    'book_list' => 'List of books',
-    'music_add' => 'Add music',
-    'music_list' => 'List of music',
-    'game_add' => 'Add game',
-    'game_list' => 'List of games',
+    'cred_head_1' => 'Developer',
+    'cred_head_2' => 'Frameworks and libraries used',
+    'cred_text_1' => 'This application is being developed by ',
+    'cred_text_2' => 'Contribute or share feedback at project\'s ',
+    'cred_github' => 'Github repository',
 
     // entity:default
     'top_cat_1' => 'Recently added',
