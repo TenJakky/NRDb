30084ef2595ef2f3a9600a8f65e00b404c682a0e
Vaclav Pelisek <peldax@gmail.com>
2017-04-16 23:54:04 +0200

    finished artist works components

--- app/component/Artist/ArtistWorks/ArtistWorks.php
+++ app/component/Artist/ArtistWorks/ArtistWorks.php
@@ -4,15 +4,16 @@ namespace App\Component;
 
 final class ArtistWorks extends BaseSmallDatagridComponent
 {
-    /** @var \App\Model\BaseRatingModel */
+    /** @var \App\Model\RatingModel */
     protected $ratingModel;
 
     /** @var int */
     protected $artistId;
 
-    public function __construct(\App\Model\ArtistEntityModel $artistEntityModel)
+    public function __construct(\App\Model\ArtistEntityModel $artistEntityModel, \App\Model\RatingModel $ratingModel)
     {
         $this->model = $artistEntityModel;
+        $this->ratingModel = $ratingModel;
     }
 
     public function render($artistId = 0)
@@ -28,15 +29,14 @@ final class ArtistWorks extends BaseSmallDatagridComponent
 
         $this->grid->addColumn('type', 'Type')->enableSort();
         $this->grid->addColumn('role', 'Role')->enableSort();
-        $this->grid->addColumn('year', 'Year')->enableSort();
         $this->grid->addColumn('original_title', 'Original title')->enableSort();
+        $this->grid->addColumn('year', 'Year')->enableSort('desc');
         $this->grid->addColumn('rating', 'Rating')->enableSort();
         $this->grid->addColumn('my_rating', 'My Rating')->enableSort();
         $this->grid->addColumn('action', 'Action');
         $this->grid->addCellsTemplate(__DIR__ . '/ArtistWorksCellsTemplate.latte');
         $this->grid->setTemplateParameters(array(
                 'ratingModel' => $this->ratingModel));
-
         return $this->grid;
     }
 
@@ -46,8 +46,18 @@ final class ArtistWorks extends BaseSmallDatagridComponent
 
         if ($order[0])
         {
-            $set->order(implode(' ', $order));
+            switch ($order[0])
+            {
+                case 'type':
+                case 'year':
+                case 'original_title':
+                case 'rating':
+                    return $set->order("entity.$order[0] $order[1]");
+                default:
+                    return $set->order(implode(' ', $order));
+            }
         }
+
         return $set;
     }
 }
