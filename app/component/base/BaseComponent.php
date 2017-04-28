c5191af327ce14eac8639f8c194136569834a0c1
Vaclav Pelisek <peldax@gmail.com>
2017-04-28 02:06:24 +0200

    chnaged directory structure

---
+++ app/component/base/BaseComponent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Component;
+
+abstract class BaseComponent extends \Nette\Application\UI\Control
+{
+    protected function getPost()
+    {
+        return $this->presenter->getPost();
+    }
+
+    public function createComponent($name, array $args = null)
+    {
+        if (method_exists($this, 'createComponent'.ucfirst($name)))
+        {
+            return parent::createComponent($name);
+        }
+        else if ($args != null)
+        {
+            return $this->presenter->getContext()->createService($name, $args);
+        }
+
+        return $this->presenter->getContext()->createService($name);
+    }
+}
