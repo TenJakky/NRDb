06067b9374ef03f53066fafabab032612be13c17
Vaclav Pelisek <peldax@gmail.com>
2017-05-11 01:48:15 +0200

    some changes with styles and asset loading

--- init.sh
+++ init.sh
@@ -7,12 +7,16 @@ echo 'Bower'
 bower install &> /dev/null
 
 echo 'Compiles SCSS'
-sassc scss/common.scss > scss/dist/common.css
+sassc scss/common.scss                      > scss/dist/common.css
+sassc scss/iframe.scss                      > scss/dist/iframe.css
+sassc scss/component/ratingForm.scss        > scss/dist/ratingForm.css
+sassc scss/component/entityList.scss        > scss/dist/entityList.css
+sassc scss/component/entitySmallList.scss   > scss/dist/entitySmallList.css
 
 echo 'Minify JS'
 uglifyjs js/common.js                       > js/dist/common.min.js
 uglifyjs js/coreValidator.js                > js/dist/coreValidator.min.js
-uglifyjs js/selectize-plugins.js            > js/dist/selectize-plugins.min.js
+uglifyjs js/selectizePlugins.js            > js/dist/selectizePlugins.min.js
 uglifyjs js/component/entityForm.js         > js/dist/entityForm.min.js
 uglifyjs js/component/statElementCount.js   > js/dist/statElementCount.min.js
 uglifyjs js/component/statUserActivity.js   > js/dist/statUserActivity.min.js
