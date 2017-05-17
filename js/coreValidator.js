74c1dc8ff72fe4604df8bd2defca23b42edcf8c3
Václav Pelíšek <peldax@gmail.com>
2017-05-17 14:02:30 +0200

    Update coreValidator.js

--- js/coreValidator.js
+++ js/coreValidator.js
@@ -19,6 +19,16 @@ Nette.validators.AppValidatorCoreValidator_lessEqual = function(elem, args, val)
 };
 
 Nette.validators.AppValidatorCoreValidator_less = function(elem, args, val)
-{
+{  
     return val < getArgsVal(elem, args);
+};
+
+Nette.validators.AppValidatorCoreValidator_length = function(elem, args, val)
+{
+    return val.length == getArgsVal(elem, args);
+};
+
+Nette.validators.AppValidatorCoreValidator_sameLength = function(elem, args, val)
+{
+    return val.length == getArgsVal(elem, args).length;
 };
