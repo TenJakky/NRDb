eccce0fb395ee94afb25aa7ed142ef7d9add860d
Václav Pelíšek <peldax@gmail.com>
2017-05-17 14:05:11 +0200

    Update CoreValidator.php

--- app/validator/CoreValidator.php
+++ app/validator/CoreValidator.php
@@ -8,6 +8,8 @@ final class CoreValidator
     const GREATER = 'App\Validator\CoreValidator::greater';
     const LESS_EQUAL = 'App\Validator\CoreValidator::lessEqual';
     const LESS = 'App\Validator\CoreValidator::less';
+    const LENGTH = 'App\Validator\CoreValidator::length';
+    const SAME_LENGTH = 'App\Validator\CoreValidator::sameLength';
 
     public static function greaterEqual(\Nette\Forms\IControl $control, string $controlName)
     {
@@ -27,5 +29,15 @@ final class CoreValidator
     public static function less(\Nette\Forms\IControl $control, string $controlName)
     {
         return $control->getValue() < $control->getForm()[$controlName]->getValue();
+    }
+    
+    public static function length(\Nette\Forms\IControl $control, string $controlName)
+    {
+        return strlen($control->getValue()) == $control->getForm()[$controlName]->getValue();
+    }
+    
+    public static function sameLength(\Nette\Forms\IControl $control, string $controlName)
+    {
+        return strlen($control->getValue()) == strlen($control->getForm()[$controlName]->getValue());
     }
 }
