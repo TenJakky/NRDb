1b5f161acab0904f8a51b885c12973f6c4b595e8
Václav Pelíšek <peldax@gmail.com>
2017-08-02 00:11:53 +0200

    security headers

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+
+declare(strict_types = 1);
+
+ini_set('session.cookie_httponly', '1');
+ini_set('session.use_only_cookies', '1');
+ini_set('session.cookie_secure', '1');
+
 $container = require __DIR__ . '/app/bootstrap.php';
 $container->getService('application')->run();
-
-
