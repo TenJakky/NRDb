77b10200f4a136a75a629125adbe2b47b2cbb3f8
Václav Pelíšek <peldax@gmail.com>
2017-08-05 19:13:02 +0200

    configuration changes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,13 +3,12 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
-
-$configurator->setDebugMode(TRUE);
+$configurator->setDebugMode(true);
 $configurator->enableDebugger(__DIR__ . '/../log');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->addConfig(__DIR__ . '/config/core.neon');
 $configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->register();
-$configurator->addConfig(__DIR__ . '/config/config.neon');
+    ->addDirectory(__DIR__)
+    ->register();
 
 return $configurator->createContainer();
